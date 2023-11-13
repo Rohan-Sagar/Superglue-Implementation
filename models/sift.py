c0074b4b592e9f1cfd7fef67d14ab365b61c587c
Rohan Sagar <rohansagar3202@gmail.com>
2023-11-13 18:06:21 -0500

    Keypoint Encoder

--- models/sift.py
+++ models/sift.py
@@ -1,4 +1,3 @@
-from sklearn.decomposition import PCA
 import cv2 as cv
 import torch
 import sys
@@ -6,7 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 import re
-from models.superglue import KeypointsMLP
+from models.superglue import KeypointEncoder
 
 input_dim = 2    # x and y coordinate
 hidden_dim = 128 
@@ -32,34 +31,15 @@ def numerical_sort(value):
     parts[1::2] = map(int, parts[1::2])
     return parts
 
-mlp = KeypointsMLP(input_dim, hidden_dim, output_dim)
-
 def process_single_image(image_path):
     img = cv.imread(image_path) # , cv.IMREAD_GRAYSCALE)
     # print(img.shape)
     fe = FeatureExtractor(img)
     kpts, des = fe.extract()
-    # print("desc", des)
-    
-    # converting kpts to tensor
-    kpts_pos = np.array([kp.pt for kp in kpts], dtype=np.float32)
-    kpts_pos_tensor = torch.tensor(kpts_pos)
-    kpt_embeddings = mlp(kpts_pos_tensor)
-    print(type(des), type(kpt_embeddings))
-
-    # embeddings_np = kpt_embeddings.detach().numpy()
-    #
-    # pca = PCA(n_components=2)
-    # reduced_embeddings = pca.fit_transform(embeddings_np)
-    #
-    # plt.scatter(reduced_embeddings[:, 0], reduced_embeddings[:, 1])
-    # plt.xlabel('Component 1')
-    # plt.ylabel('Component 2')
-    # plt.title('PCA of Keypoint Embeddings')
-    # plt.show()
-
+    ke = KeypointEncoder(kpts, des)
+    output = ke(kpts, des) 
     img_kpts = draw_keypoints(img, kpts)
-    return img_kpts, kpts, des, kpt_embeddings
+    return img_kpts, kpts, des 
 
 def process_images(input_directory, output_directory):
     image_files = sorted([f for f in os.listdir(input_directory) if f.endswith(('.png', '.jpg', '.jpeg'))], key = numerical_sort)
@@ -68,8 +48,8 @@ def process_images(input_directory, output_directory):
         img1_path = os.path.join(input_directory, image_files[i])
         img2_path = os.path.join(input_directory, image_files[i+1])
 
-        img1, kp1, des1, embed1 = process_single_image(img1_path)
-        img2, kp2, des2, embed1 = process_single_image(img2_path)
+        img1, kp1, des1 = process_single_image(img1_path)
+        img2, kp2, des2 = process_single_image(img2_path)
 
         combined_image = np.hstack((img1, img2))
         # output_path = os.path.join(output_directory, f'combined_{i}.png')
--- models/superglue.py
+++ models/superglue.py
@@ -1,5 +1,7 @@
 import torch
 import torch.nn as nn
+from sklearn.decomposition import PCA
+import numpy as np
 
 class KeypointsMLP(nn.Module):
     def __init__(self, input_dim, hidden_dim, output_dim):
@@ -7,10 +9,45 @@ class KeypointsMLP(nn.Module):
         self.layers = nn.Sequential(
             nn.Linear(input_dim, hidden_dim),
             nn.ReLU(),
-            nn.Linear(hidden_dim, hidden_dim),
-            nn.ReLU(),
             nn.Linear(hidden_dim, output_dim)
         )
 
     def forward(self, x):
         return self.layers(x)
+
+input_dim = 2       # (x,y)
+hidden_dim = 128
+output_dim = 128    # 128-dimensional vector
+
+mlp = KeypointsMLP(input_dim, hidden_dim, output_dim)
+
+class KeypointEncoder(nn.Module):
+    def __init__(self, kpts, des):
+        super(KeypointEncoder, self).__init__()
+        self.kpts = kpts
+        self.des = des
+
+    def forward(self, kpts, des):
+        # print(type(kpts))
+        # converting kpts to tensor
+        kpts_pos = np.array([kp.pt for kp in kpts], dtype=np.float32)
+        kpts_pos_tensor = torch.tensor(kpts_pos)
+        # print(kpts_pos_tensor.shape)
+        kpt_embeddings = mlp(kpts_pos_tensor)
+
+        # embeddings_np = kpt_embeddings.detach().numpy()
+        #
+        # pca = PCA(n_components=2)
+        # reduced_embeddings = pca.fit_transform(embeddings_np)
+        #
+        # plt.scatter(reduced_embeddings[:, 0], reduced_embeddings[:, 1])
+        # plt.xlabel('Component 1')
+        # plt.ylabel('Component 2')
+        # plt.title('PCA of Keypoint Embeddings')
+        # plt.show()
+        
+        des_to_tensor = torch.from_numpy(des)
+        # print(des_to_tensor.shape)
+        output = des_to_tensor + kpt_embeddings
+        # print(output)
+        return output
